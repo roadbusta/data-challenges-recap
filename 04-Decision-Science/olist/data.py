16ff557c6e16c2b5bba1ae9217c9b55e97baba34
kevin robert <kevin.j.robert@gmail.com>
2022-01-20 09:42:44 +0100

    breakpoint in DS/olist/data.py

--- 04-Decision-Science/olist/data.py
+++ 04-Decision-Science/olist/data.py
@@ -12,7 +12,7 @@ class Olist:
         # Hints 1: Build csv_path as "absolute path" in order to call this method from anywhere.
             # Do not hardcode your path as it only works on your machine ('Users/username/code...')
             # Use __file__ instead as an absolute path anchor independant of your usename
-            # Make extensive use of `import ipdb; ipdb.set_trace()` to investigate what `__file__` variable is really
+            # Make extensive use of `breakpoint()` to investigate what `__file__` variable is really
         # Hint 2: Use os.path library to construct path independent of Mac vs. Unix vs. Windows specificities
         pass  # YOUR CODE HERE
 
