4c560900459399b5e1dc617b52708bc09ee5a1a9
Bruno Lajoie <brunolajoie@gmail.com>
2022-02-15 11:47:24 +0100

    update 0301-Algebra

--- 03-Maths/01-Algebra-Calculus/02-real-estate-advanced-estimator/tests/test_univariate.py
+++ 03-Maths/01-Algebra-Calculus/02-real-estate-advanced-estimator/tests/test_univariate.py
@@ -2,16 +2,16 @@ from nbresult import ChallengeResultTestCase
 
 
 class TestUnivariate(ChallengeResultTestCase):
-    def test_squared_errors(self):
-        self.assertEqual(list(self.result.squared_errors), [0, 0])
 
     def test_mse(self):
-        self.assertEqual(self.result.mse, 0)
+        self.assertLess(self.result.mse_min, 65000)
 
     def test_best_slope(self):
-        self.assertGreater(self.result.theta1, 0)
-        self.assertGreater(0.5, self.result.theta1)
+        self.assertGreater(self.result.slope_best, 0.10)
+        self.assertLess(self.result.slope_best, 0.40)
 
     def test_best_intercept(self):
-        self.assertGreater(self.result.theta0, -100)
-        self.assertGreater(100, self.result.theta0)
+        self.assertGreater(self.result.intercept_best, -70)
+        self.assertLess(self.result.intercept_best, 40)
+
+
