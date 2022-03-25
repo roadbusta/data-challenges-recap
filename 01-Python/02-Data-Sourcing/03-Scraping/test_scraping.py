c5f76988f345b014a5e3dfc2869c31979cd4048d
Joseph Gulay <joegulay@gmail.com>
2022-03-25 18:12:02 +1100

    Complete this code

--- 01-Python/02-Data-Sourcing/03-Scraping/test_scraping.py
+++ 01-Python/02-Data-Sourcing/03-Scraping/test_scraping.py
@@ -2,3 +2,9 @@
 
 # TODO: paste the code from Kitt's instructions
 
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(open("pages/carrot.html"), "html.parser")
+
+for recipe in soup.find_all('p', class_='recipe-name'):
+    print(recipe.text)
