41c6ec044abfd78daec667e8497adfdaf70a1cda
Joseph Gulay <joegulay@gmail.com>
2022-03-25 17:13:53 +1100

    Complete programming basics

--- 01-Python/01-Programming-Basics/06-Environment/flask_option.py
+++ 01-Python/01-Programming-Basics/06-Environment/flask_option.py
@@ -1,10 +1,48 @@
 # pylint: disable=missing-docstring
 
-import os
+# import os
+
+# def start():
+#     """returns the right message"""
+#     try:
+#         return "Starting in " + os.environ['FLASK_ENV'] + " mode..."
+
+#     except KeyError:
+#         return "Starting in production mode..."
+
+
+
+
+# if __name__ == "__main__":
+#     print(start())
+
+
+# import os
+
 
 def start():
     """returns the right message"""
-    pass  # YOUR CODE HERE
+    # $CHALLENGIFY_BEGIN
+    env = os.getenv('FLASK_ENV')
+    if env:
+        return f"Starting in {env} mode..."
+    return "Starting in production mode..."
+    # $CHALLENGIFY_END
+
+
+if __name__ == "__main__":
+    print(start())
+
+import os
+
+
+def start():
+    env = os.environ['FLASK_ENV']
+    if env:
+        return f"Starting in {env} mode..."
+    else:
+        return "Starting in production mode..."
+
 
 if __name__ == "__main__":
     print(start())
--- 01-Python/02-Data-Sourcing/02-API/test_api.py
+++ 01-Python/02-Data-Sourcing/02-API/test_api.py
@@ -2,3 +2,9 @@
 
 # TODO: paste the code from Kitt's instructions
 
+import requests
+
+url = "https://www.metaweather.com/api/location/search/?query=london"
+response = requests.get(url).json()
+city = response[0]
+print(f"{city['title']}: {city['woeid']} ({city['latt_long']})")
