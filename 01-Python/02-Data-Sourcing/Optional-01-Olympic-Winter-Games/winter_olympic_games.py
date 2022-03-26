2fb0d99a74f595c42b9306c476c9d7abd1afa99b
Joseph Gulay <joegulay@gmail.com>
2022-03-26 12:05:26 +1100

    modify winter_olympic_games.py

--- 01-Python/02-Data-Sourcing/Optional-01-Olympic-Winter-Games/winter_olympic_games.py
+++ 01-Python/02-Data-Sourcing/Optional-01-Olympic-Winter-Games/winter_olympic_games.py
@@ -8,7 +8,18 @@ MEDALS_FILEPATH = "data/winter.csv"
 
 def most_decorated_athlete_ever():
     """Returns who won the most winter olympic games medals (gold/silver/bronze) ever"""
-    pass  # YOUR CODE HERE
+    # Create a winter olypmics list of dictionary
+    medal_count = {}
+    with open(MEDALS_FILEPATH, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if row["Athlete"] in medal_count.keys():
+                medal_count[row["Athlete"]] +=1
+            else:
+                medal_count[row["Athlete"]] = 1
+
+    sorted_medals = sorted(medal_count.items(), key=lambda x: x[1], reverse=True) #Creates a list
+    return sorted_medals[0][0]
 
 
 def country_with_most_gold_medals(min_year, max_year):
@@ -18,3 +29,6 @@ def country_with_most_gold_medals(min_year, max_year):
 def top_three_women_in_five_thousand_meters():
     """Returns the three women with the most 5000 meters medals(gold/silver/bronze)"""
     pass  # YOUR CODE HERE
+
+if __name__ == "__main__":
+    print(most_decorated_athlete_ever())
