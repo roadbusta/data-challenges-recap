c51f0c85e9068c02bc7cfd4e01409c0052ef3c30
Joseph Gulay <joegulay@gmail.com>
2022-03-27 17:39:34 +1100

    Performed live code with group

--- 01-Python/02-Data-Sourcing/demo.py
+++ 01-Python/02-Data-Sourcing/demo.py
@@ -1,3 +1,17 @@
+#import libraries
+import requests
+from bs4 import BeautifulSoup
 
-name = Joe
-print("My name is name")
+url= "https://www.imdb.com/list/ls055386972/"
+
+response = requests.get(url)
+
+soup  = BeautifulSoup(response.content , "html.parser")
+
+
+section_list = soup.find_all("div", class_="lister-item-content")
+
+for section in section_list:
+    title = section.find("a").string
+    print(type(title))
+    print(title)
