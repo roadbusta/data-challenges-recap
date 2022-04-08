4d9dffef250955348b590d4b3d0b0ce79f19e7e4
Joseph Gulay <joegulay@gmail.com>
2022-04-08 21:16:27 +1000

    Completed first challenge

--- 02-Data-Toolkit/02-Data-Sourcing/00-Warmup/opengraph.py
+++ 02-Data-Toolkit/02-Data-Sourcing/00-Warmup/opengraph.py
@@ -9,9 +9,18 @@ def fetch_metadata(url):
     """
     Return a dictionary of OpenGraph metadata found in HTML of given url
     """
-    pass  # YOUR CODE HERE
+    base_URI= "https://opengraph.lewagon.com/"
+    params = {"url" : f'{url}'}
+    response = requests.get(base_URI, params)
+    if response.status_code == 200:
+        return response.json()
+
+    return None
+
+
 
 # To manually test, please uncomment the following lines and run `python opengraph.py`:
 # import pprint
 # pp = pprint.PrettyPrinter(indent=4)
+
 # pp.pprint(fetch_metadata("https://www.lewagon.com"))
